7d4a2f48884f8cf04dba5c28b5dc630f3d5719fa
bnicp <bnp415@gmail.com>
2022-03-21 14:54:03 -0400

    added functions for types of characters

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
-// Assignment Code
+// char set - http://www.net-comber.com/charset.html
+
+
+// Assignment Code -- PROVIDED BY TA
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// assigning variables
+var length = document.querySelector('#numCharacters');
+var lowercase = document.querySelector('#incLowercase');
+var uppercase = document.querySelector('#incUppercase');
+var numbers = document.querySelector('#incNumbers');
+var symbols = document.querySelector('#incSymbols');
+var form = document.querySelector('#passwordForm');
+
+//function to generate password
+function generatePassword() {
+
+}
+
+// Write password to the #password input -- PROVIDED BY TA
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+//next thing that needs to be done
+
+  
+
+
+// Add event listener to generate button -- PROVIDED BY TA
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// generator functions
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	return +String.fromCharCode(Math.floor(Math.random() * ) + );
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
