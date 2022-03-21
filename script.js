ddc176921c2d8adbe2d11e2ce5aea4654ed35a28
bnicp <bnp415@gmail.com>
2022-03-21 19:04:18 -0400

    worked out bugs with mismatched classes in html/JS and added error popup for passwords outside 8-128 characters

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var symbols = document.getElementById('incSymbols');
 var form = document.getElementById('passwordForm');
 var passwordText = document.getElementById('password'); 
 
+console.log(uppercase);
+
 const randomChoices = {
   lowercase: getRandomLower,
   uppercase: getRandomUpper,
@@ -24,44 +26,48 @@ const randomChoices = {
 generateBtn.addEventListener("click", () => {
   var numCharacters = +length.value;
   var incUppercase = uppercase.checked;
+  var incLowercase = lowercase.checked;
   var incNumbers = numbers.checked;
   var incSymbols = symbols.checked;
 
-  passwordText.innerText = generatePassword(numCharacters, incUppercase, incNumbers, incSymbols, length);
+if(numCharacters <8 || numCharacters >128) {
+  return alert("Please enter a number from 8-128 characters.")
+} else {
+  passwordText.innerText = generatePassword(numCharacters, incLowercase, incUppercase, incNumbers, incSymbols);}
+
 });
 
 // generate password function
-function generatePassword(length, lowercase, uppercase, numbers, symbols); {
+function generatePassword(length, lowercase, uppercase, numbers, symbols) {
   
-  // build password from strings
+// build password from strings
   let generatedPassword = '';
 
   var incParameters = lowercase + uppercase + numbers + symbols;
   
-  //don't include unchecked parameters
-  var incArray = [{lowercase}, {uppercase}, {numbers}, {symbols}].filter (item => Object.values(item)[0];
+//don't include unchecked parameters
+  var incArray = [{lowercase}, {uppercase}, {numbers}, {symbols}].filter(item => Object.values(item)[0])
 
   if(incArray === 0) {
     return '';
   }
 
-  // loop through length and generate each accepted parameter
+// loop through length and generate each accepted parameter
   for(let i= 0; i < length; i+= incParameters) {
     incArray.forEach(type => {
-      const keys = object.keys(type)[0];
+      const keys = Object.keys(type)[0];
 
       generatedPassword += randomChoices[keys]();
     });
   }
-  //write to page
+//write to page
   const output = generatedPassword.slice(0, length);
 	
 	return output;
 }
 
 
-
-
+// generator functions
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
